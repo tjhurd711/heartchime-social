5085275cbdefad36ecced3af85de0f00eb4db2bb
Tyler Hurd <tyler.j.hurd@gmail.com>
2026-05-20 13:31:11 -0500

    Include chromium binary in serverless bundle

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['@sparticuz/chromium'],
+  outputFileTracingIncludes: {
+    '/*': [
+      './node_modules/@sparticuz/chromium/bin/**',
+    ],
+  },
 }
 
 export default nextConfig
